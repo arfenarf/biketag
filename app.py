c044950c9c12160a98b7ee3a2d8d53d2ce97d91b
Kate <kgweber@umich.edu>
2023-12-19 17:11:27 -0500

    Two-tabbed version

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
-from flask import Flask, render_template, request, url_for
-from maps import load_and_build_map, build_map
-from flask_wtf import FlaskForm
-from wtforms import SelectMultipleField, SubmitField
-from flask_sqlalchemy import SQLAlchemy
 import os
+from datetime import datetime
+
+import pandas as pd
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from sqlalchemy import Integer, String, Float, DateTime
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from wtforms import SelectMultipleField, SubmitField
+
+from maps import load_and_build_map, build_map
+from plots import create_bokeh_network, create_plotly_distograms, create_plotly_heatmap
 
 
 class Base(DeclarativeBase):
     pass
 
-db = SQLAlchemy(model_class=Base)
 
-SECRET_KEY = os.urandom(32)
+db = SQLAlchemy(model_class=Base)
 
 
 class PersonForm(FlaskForm):
     prefix = "person"
     options = SelectMultipleField("Select person(s)")
-    peoplesubmit = SubmitField("Submit")
 
-
-app = Flask(__name__)
-app.config["SECRET_KEY"] = SECRET_KEY
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-
-from sqlalchemy import Integer, String, Float
-from sqlalchemy.orm import Mapped, mapped_column
+    peoplesubmit = SubmitField("Update Map")
 
 
 class Person(db.Model):
@@ -39,69 +36,104 @@ class Person(db.Model):
     location_density: Mapped[float] = mapped_column(Float, nullable=True)
     location_count: Mapped[int] = mapped_column(Integer)
 
+    def to_dict(self):
+        return {field.name: getattr(self, field.name) for field in self.__table__.c}
+
+
+class Tag(db.Model):
+    tag_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    placed_by: Mapped[str] = mapped_column(String, nullable=True)
+    found_by: Mapped[str] = mapped_column(String, nullable=True)
+    placed_by_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    found_by_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    placed_at_datetime: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    found_at_datetime: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    drive_distance_mi: Mapped[float] = mapped_column(Float, nullable=True)
+    crow_distance_mi: Mapped[float] = mapped_column(Float, nullable=True)
+
+    def to_dict(self):
+        return {field.name: getattr(self, field.name) for field in self.__table__.c}
+
+
+SECRET_KEY = os.urandom(32)
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 with app.app_context():
     db.create_all()
     db.session.commit()
-    m, map_df, person_df, gdf = load_and_build_map(
-        "data/biketag.tsv", "data/person_nodes.csv"
-    )
-    print(person_df.columns)
-    print(person_df.head())
+    m, map_df, person_df, gdf = load_and_build_map("data/biketag.tsv")
+
     for person, row in person_df.iterrows():
-        p = Person(
-            person_id=row.person_id,
-            person=person,
-            cluster=row.cluster,
-            convex_hull_area=round(row.convex_hull_area, 2),
-            location_density=round(row.location_density, 4),
-            location_count=row.location_count,
-        )
+        p = Person(person_id=row.person_id, person=person, cluster=row.cluster_label,
+                   convex_hull_area=round(row.convex_hull_area, 2), location_density=round(row.location_density, 4),
+                   location_count=row.location_count, )
         db.session.add(p)
+
     db.session.commit()
+
+    for tag, row in gdf.iterrows():
+        t = Tag(tag_id=row.tag_id, name=row['name'], placed_by=row.placed_by, found_by=row.found_by,
+                placed_by_id=row.placed_by_id, found_by_id=row.found_by_id,
+                placed_at_datetime=datetime.fromisoformat(row.placed_at_datetime),
+                found_at_datetime=datetime.fromisoformat(row.found_at_datetime),
+                drive_distance_mi=row.drive_distance_mi, crow_distance_mi=row.crow_dist_mi)
+        db.session.add(t)
+    db.session.commit()
+
+    print("this is just for debugging")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     sort = request.args.get("sort", "person_id")
     reverse = request.args.get("direction", "asc") == "desc"
-    m, map_df, person_df, gdf = load_and_build_map(
-        "data/biketag.tsv", "data/person_nodes.csv"
-    )
+    m, map_df, person_df, gdf = load_and_build_map("data/biketag.tsv")
 
     peopleform = PersonForm()
     people = Person.query
     selector_options = [(person.person, person.person) for person in people if person.location_count > 2]
-    # people = [(person, person) for person in map_df.person.unique()]
     selector_options.sort()
     peopleform.options.choices = selector_options
 
     if peopleform.validate_on_submit() and peopleform.peoplesubmit.data:
-        print(peopleform.options.data)
-        m = build_map(
-            map_df.loc[map_df.person.isin(peopleform.options.data)], person_df, gdf
-        )
+
+        if peopleform.options.data == []:
+            peopleform.options.data = [person.person for person in people if person.location_count > 2]
+        m = build_map(map_df, person_df, gdf, peopleform=peopleform)
 
     # set the iframe width and height
     m.get_root().width = "800px"
     m.get_root().height = "600px"
     iframe = m.get_root()._repr_html_()
 
-    return render_template(
-        "iframe.html",
-        title="Bike Tag Map!",
-        iframe=iframe,
-        peopleform=peopleform,
-        peopletable=people
-    )
+    return render_template("map_table.html", title="Bike Tag Map!", iframe=iframe, peopleform=peopleform,
+                           peopletable=people)
 
 
-@app.route("/item/<int:id>")
-def flask_link(id):
-    element = get_element_by_id(id)
-    return "<h1>{}</h1><p>{}</p><hr><small>id: {}</small>".format(
-        element.name, element.description, element.id
-    )
+@app.route("/plots", methods=["GET"])
+def show_plots():
+    tags = Tag.query
+    tag_df = pd.DataFrame([tag.to_dict() for tag in tags]).set_index("tag_id")
+
+    # draw_network
+    network_script, network_div = create_bokeh_network(person_df, tag_df)
+
+    # time to find hist
+
+    # distance hist
+    distograms = create_plotly_distograms(tag_df)
+
+    # frequency by month-year
+    datemap = create_plotly_heatmap(tag_df)
+
+    return render_template("plots.html", title="Bike Tag Plots", script=network_script, div=network_div,
+                           distograms=distograms, datemap=datemap, )
 
 
 if __name__ == "__main__":
--- maps.py
+++ maps.py
@@ -1,161 +1,155 @@
-import geopandas as gpd
-import pandas as pd
 import branca.colormap as cm
+import community as community_louvain
 import folium
+import geopandas as gpd
+import networkx as nx
+import pandas as pd
 import requests
 
 
 def load_data(filepath="data/biketag.tsv", sep="\t"):
     """Load the bike-tag data from a TSV copy of the Google Sheet"""
     r = requests.get(
-        "https://docs.google.com/spreadsheets/d/1ngFLRbIZAnWOyczNyph2WIQlysRoTJC2kkPqgNCpkoE/export?format=tsv"
-    )
+        "https://docs.google.com/spreadsheets/d/1ngFLRbIZAnWOyczNyph2WIQlysRoTJC2kkPqgNCpkoE/export?format=tsv")
 
     with open(filepath, "w") as f:
         f.write(r.text)
 
     biketag = pd.read_csv(filepath, sep=sep, encoding="utf-8")
-    gdf = gpd.GeoDataFrame(
-        biketag,
-        geometry=gpd.points_from_xy(biketag.longitude, biketag.latitude),
-        crs="EPSG:4326",
-    )
+    gdf = gpd.GeoDataFrame(biketag, geometry=gpd.points_from_xy(biketag.longitude, biketag.latitude), crs="EPSG:4326", )
     return gdf
 
 
-def create_person_df(cluster_filepath, gdf, sep=","):
-    person_df = pd.concat(
-        [
-            gdf[["placed_by", "geometry"]].rename(columns={"placed_by": "person"}),
-            gdf[["found_by", "geometry"]].rename(columns={"found_by": "person"}),
-        ]
-    )
+def create_person_df(gdf, sep=","):
+    person_df = pd.concat([gdf[["placed_by", "geometry"]].rename(columns={"placed_by": "person"}),
+                           gdf[["found_by", "geometry"]].rename(columns={"found_by": "person"}), ])
     person_df["location_count"] = 1
     # and now we roll it up
     person_df = person_df.dissolve(by="person", aggfunc="count")
-
-    # graft in some graph smartness
-    cluster_info = pd.read_csv(cluster_filepath, sep=sep)
-    person_df = person_df.join(
-        cluster_info[["Label", "modularity_class"]].set_index("Label")
-    )
-    person_df.modularity_class = person_df.modularity_class.astype("category")
-    person_df = person_df.rename(columns={"modularity_class": "cluster"})
-
-    # print(person_df.head())
 
     # add the convex and concave hulls
     person_df["convex_hull"] = person_df.geometry.convex_hull
     person_df["concave_hull"] = person_df.geometry.concave_hull()
 
+    # let's get the center of the convex hull
+    person_df["convex_hull_center"] = person_df.convex_hull.to_crs("+proj=cea").centroid
+
     # force an area calculation in square miles
-    person_df["convex_hull_area"] = (
-        person_df.convex_hull.to_crs("+proj=cea").area * 3.861e-7
-    )
-    person_df["location_density"] = (
-        person_df.location_count / person_df.convex_hull_area
-    )
+    person_df["convex_hull_area"] = (person_df.convex_hull.to_crs("+proj=cea").area * 3.861e-7)
+    person_df["location_density"] = (person_df.location_count / person_df.convex_hull_area)
     person_df["person_id"] = range(len(person_df))
 
     return person_df
 
 
 def merge_person_data(gdf, person_df):
-    gdf = gdf.merge(
-        person_df[["person_id"]], left_on="placed_by", right_index=True
-    ).rename(columns={"person_id": "placed_by_id"})
-    gdf = gdf.merge(
-        person_df[["person_id"]], left_on="found_by", right_index=True
-    ).rename(columns={"person_id": "found_by_id"})
+    gdf = gdf.merge(person_df[["person_id"]], left_on="placed_by", right_index=True).rename(
+        columns={"person_id": "placed_by_id"})
+    gdf = gdf.merge(person_df[["person_id"]], left_on="found_by", right_index=True).rename(
+        columns={"person_id": "found_by_id"})
     return gdf
 
 
+def cluster_people(gdf, person_df):
+    # create a networkx graph
+    G = nx.Graph()
+
+    # add the edges
+    for index, row in gdf.iterrows():
+        G.add_edge(row.placed_by_id, row.found_by_id)
+
+    # run the clustering algorithm
+    partition = community_louvain.best_partition(G, random_state=42)
+    person_df['cluster'] = person_df.person_id.apply(lambda x: partition[x])
+    person_df['cluster_label'] = person_df.cluster.apply(lambda x: f"C-{x}")
+
+    return person_df, {'graph': G, 'partition': partition}
+
+
 def create_mapping_df(person_df):
-    mapping_df = (
-        person_df.loc[person_df.location_count > 2]
-        .reset_index()
-        .set_geometry("convex_hull")
-        .sort_values(by="convex_hull_area", ascending=False)
-    )
+    mapping_df = (person_df.loc[person_df.location_count > 2].reset_index().set_geometry("convex_hull").sort_values(
+        by="convex_hull_area", ascending=False))
     return mapping_df
 
 
-def build_map(map_df, person_df, gdf, person_subset=None):
-    person_subset = (
-        list(map_df.person.unique()) if person_subset is None else person_subset
-    )
-    map_df = map_df.loc[map_df.person.isin(person_subset)]
+def build_map(map_df, person_df, gdf, peopleform=None):
+    if peopleform:
+        map_df = map_df.loc[map_df.person.isin(peopleform.options.data)]
 
-    colors = cm.linear.Set1_08.to_step(n=person_df.person_id.max()).scale(
-        0, person_df.person_id.max()
-    )
-    map_df["__color"] = map_df.person_id.apply(colors)
+    # viridis_colors = cm.linear.Set1_08.to_step(n=person_df.person_id.max()).scale(0, person_df.person_id.max())
+    step = cm.linear.Set1_08.to_step(person_df.cluster.max()).scale(0, 5)
+    map_df["__color"] = map_df.cluster.apply(step)
 
     # create the map object
-    m = folium.Map(
-        location=[42.3312832, -83.79662475],
-        zoom_start=11,
-        tiles="https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=5d38772f44bf4755bd8859e5bf9ef4a5",
-        attr='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    )
+    m = folium.Map(location=[42.3312832, -83.79662475], zoom_start=11, tiles=None)
+
+    folium.TileLayer(name="OpenCycleMap",
+                     tiles="https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=5d38772f44bf4755bd8859e5bf9ef4a5",
+                     attr='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+                     overlay=False, control=True, ).add_to(m)
 
     # add the hull shapes
-    folium.GeoJson(
-        map_df[
-            [
-                "person",
-                "convex_hull",
-                "convex_hull_area",
-                "cluster",
-                "location_density",
-                "__color",
-            ]
-        ],
-        style_function=lambda x: {
-            "fillColor": x["properties"]["__color"],
-            "color": "grey",
-            "weight": 1,
-            "fillOpacity": 0.3,
-        },
-        tooltip=folium.GeoJsonTooltip(
-            fields=["person", "cluster", "convex_hull_area", "location_density"],
-            aliases=["Person Name", "cluster", "Area (sq. mi.)", "Tags/mi^2"],
-        ),
-    ).add_to(m)
+    h = folium.GeoJson(map_df[["person", "convex_hull", "convex_hull_area", "cluster", "location_density", "__color", ]],
+                   name="Rider Coverage Area",
+                   style_function=lambda x: {"fillColor": x["properties"]["__color"], "color": "grey", "weight": 1,
+                                             "fillOpacity": 0.25, },
+                   tooltip=folium.GeoJsonTooltip(fields=["person", "cluster", "convex_hull_area", "location_density"],
+                                                 aliases=["Person Name", "cluster", "Area (sq. mi.)",
+                                                          "Tags/mi^2"], ), )
+    h.add_to(m)
 
     # add the tag locations
-    folium.GeoJson(
-        gdf,
-        name="Tags Placed",
-        marker=folium.CircleMarker(
-            radius=5, fill_color="blue", fill_opacity=0.3, color="black", weight=1
-        ),
-        tooltip=folium.GeoJsonTooltip(
-            fields=["name", "placed_by", "found_by"],
-            aliases=["Location Name", "Placed By", "Found By"],
-        ),
-        popup=folium.GeoJsonPopup(
-            fields=["name", "placed_by", "found_by"],
-            aliases=["Location Name", "Placed By", "Found By"],
-        ),
-        style_function=lambda x: {
-            "fillColor": colors(x["properties"]["placed_by_id"]),
-        },
-    ).add_to(m)
+    if peopleform:
+        display_gdf = gdf.loc[
+            (gdf.placed_by.isin(peopleform.options.data)) | (gdf.found_by.isin(peopleform.options.data))]
 
-    m.save("templates/biketag.html")
+    else:
+        display_gdf = gdf.copy()
+
+    if len(display_gdf) > 0:
+        folium.GeoJson(display_gdf, name="Tags Placed",
+                       marker=folium.CircleMarker(radius=3, fill_color="darkblue", fill_opacity=1, color="grey",
+                                                  weight=1),
+                       tooltip=folium.GeoJsonTooltip(fields=["name", "placed_by", "found_by"],
+                                                     aliases=["Location Name", "Placed By", "Found By"], ),
+                       popup=folium.GeoJsonPopup(fields=["name", "placed_by", "found_by"],
+                                                 aliases=["Location Name", "Placed By", "Found By"], )).add_to(m)
+
+    # add the centroids
+    if peopleform:
+        centroid_df = person_df.loc[peopleform.options.data]
+
+    else:  # default to showing centroids
+        centroid_df = person_df.copy()
+
+    if len(centroid_df) > 0:
+        centroid_df = centroid_df.set_geometry("convex_hull_center", drop=True)
+        centroid_df = centroid_df[["cluster", "cluster_label", "person_id", 'geometry']].reset_index()
+        centroid_df["__color"] = centroid_df.cluster.apply(step)
+        z = folium.GeoJson(centroid_df, name="Centroids",
+                           marker=folium.CircleMarker(radius=6, fill_color = 'green', fill_opacity=1, color="black", weight=1),
+                           tooltip=folium.GeoJsonTooltip(fields=["person", "cluster_label"], aliases=["Centroid for:", "Cluster:"]),
+                           popup=folium.GeoJsonPopup(fields=["person", "cluster_label"], aliases=["Centroid for:", "Cluster:"]),
+                           style_function=lambda x: {"fillColor": x["properties"]["__color"], "color": "black",
+                                                     "weight": 1,
+                                                     "fillOpacity": 0.7, },)
+
+        z.add_to(m)
+
+    folium.LayerControl().add_to(m)
+    m.save("assets/biketag.html")
     return m
 
 
-def load_and_build_map(biketag_filepath, cluster_filepath):
+def load_and_build_map(biketag_filepath):
     gdf = load_data(biketag_filepath)
-    person_df = create_person_df(cluster_filepath, gdf)
+    person_df = create_person_df(gdf)
     gdf = merge_person_data(gdf, person_df)
+    person_df, _ = cluster_people(gdf, person_df)
     map_df = create_mapping_df(person_df)
     m = build_map(map_df, person_df, gdf)
-    # load_table(person_df)
     return m, map_df, person_df, gdf
 
 
 if __name__ == "__main__":
-    load_and_build_map("data/biketag.tsv", "data/person_nodes.csv")
+    load_and_build_map("data/biketag.tsv")
